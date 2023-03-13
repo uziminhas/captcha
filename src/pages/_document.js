937dbabaaec909076351e4ef0292860e45a90780
Uzair Minhas <uziminhas@gmail.com>
2023-03-12 20:17:00 -0400

    Add Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,6 +33,23 @@ export default function Document() {
           content="https://captiongen.io/preview.png"
         />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
       </Head>
       <body>
         <Main />
